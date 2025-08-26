de1c1a157db9247e138565e4a79be1a1a421d1a7
GersonESS <gebhsantos@gmail.com>
2025-08-26 06:56:15 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiUrl = 'https://api-produto-41km.onrender.com/produtoMongo'; // A API será servida no mesmo host
+    const apiUrl = 'http://api-produto-41km.onrender.com/produtoMongo'; // A API será servida no mesmo host
  
     const formAdicionar = document.getElementById('form-adicionar');
     const tabelaprodutosBody = document.querySelector('#tabela-produtos tbody');
